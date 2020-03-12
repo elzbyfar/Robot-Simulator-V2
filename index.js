4b2e1c266ba70c2521d61a7af8a7a39b7da7ec9d
github username <github email address>
2020-03-12 11:51:10 -0400

    Works, but could not stop interval from counting. That is only visible from the console though.

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
+let moveButton = document.getElementById('move-button')
+let movesList = document.getElementById("moves-container")
+
+
 document.addEventListener("DOMContentLoaded", function(){
   createGrid()
   renderBot(currentPosition)
+  
+  document.addEventListener("keydown", function(event){
+    if (event.keyCode === 37) {
+      let li = document.createElement('li')
+      li.innerText = "left"
+      movesList.appendChild(li)
+    }
+    
+    if (event.keyCode === 38) {
+      let li = document.createElement('li')
+      li.innerText = "up"
+      movesList.appendChild(li)
+    }
 
-  // ADD CODE HERE!
+    if (event.keyCode === 39) {
+      let li = document.createElement('li')
+      li.innerText = "right"
+      movesList.appendChild(li)
+    }
+
+    if (event.keyCode === 40) {
+      let li = document.createElement('li')
+      li.innerText = "down"
+      movesList.appendChild(li)
+    }
+
+    if (event.keyCode === 8) {
+      let allMoves = Array.from(movesList.childNodes)
+      allMoves.pop().remove()
+    }
+  })
+
+  document.addEventListener('click', function(event){
+    let allMoves = Array.from(movesList.childNodes)
+
+    function makeMoves() {
+      move(allMoves[0].innerText);
+      allMoves.shift().remove();
+    }
+
+    if (allMoves.length > 0) {
+      window.setInterval(makeMoves, 500)
+    } else {
+      clearInterval(makeMoves)
+    }
+  })
 })
